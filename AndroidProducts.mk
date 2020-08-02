0f9b9a8ca4e6c03717b4d180da1ef6058b760dc5
ROXAANE <bajithottyadvaith99@gmail.com>
2020-08-02 11:05:55 +0000

    sanders: Init Fuxk
    Signed-off-by: ROXAANE <bajithottyadvaith99@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/xtended_sanders.mk
+	$(LOCAL_DIR)/aosp_sanders.mk
 
 COMMON_LUNCH_CHOICES := \
-	xtended_sanders-userdebug
+	aosp_sanders-userdebug
--- aosp_sanders.mk
+++ aosp_sanders.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 $(call inherit-product, device/motorola/sanders/device.mk)
 
 # Inherit some cmmon xtended stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
@@ -32,7 +32,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
-PRODUCT_NAME := xtended_sanders
+PRODUCT_NAME := aosp_sanders
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
